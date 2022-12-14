95fa5d8b1a43d2b79f70ea139d6d1b23cbc5dd90
Stephania Grenfell <StephaniaGrenfell@gmail.com>
2022-12-13 19:17:02 -0600

    Functioning as intended

--- module-1-capstone/capstone-andrew/src/main/java/com/techelevator/exception/NoSuchSlotException.java
+++
@@ -1,8 +0,0 @@
-package com.techelevator.exception;
-
-public class NoSuchSlotException extends RuntimeException {
-
-    public NoSuchSlotException(String slot) {
-        super("No such slot = " + slot);
-    }
-}
